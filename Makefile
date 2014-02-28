d561b6f559cb2fcf3ec3eb7e8df3a6872fab9e04
Roberto Medina <robertoxmed@gmail.com>
2014-02-28 19:46:34 +0100

    Quelques changements de la liste chaînée

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
-LIB = ./lib
-PAPI_LIB = ./lib/libpapi.a
+LIB = lib
+PAPI_LIB = lib/libpapi.a
 CFLAGS = -Wall -g -D_GNU_SOURCE
 
 CC=gcc $(CFLAGS)
 
-BIN=./bin
-SRC=./src
-OBJ=./obj
-INC=./include
+BIN=bin
+SRC=src
+OBJ=obj
+INC=include
 
 EXEC=papi_wrapper
 TAR = PSAR
--- src/rt_task.c
+++ src/rt_task.c
@@ -1,109 +1,106 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "../include/rt_task.h"
 
-#define SIZE 3
+void rt_task_init(rt_list *list){
 
-typedef struct op{
-  int mat[SIZE][SIZE];
-  struct op *next;
-}list_op;
+    rt_element *op_1 = NULL;
+    rt_element *op_2 = NULL;    
+    int i,j;
 
-/*Paramètre ite = nombre de produits matriciels à effectuer */
-int rt_task(int ite){
+    op_1 = (rt_element *)malloc(sizeof(rt_element));
+    op_2 = (rt_element *)malloc(sizeof(rt_element));
 
-  list_op *head = NULL;
-  list_op *op_1 = NULL;
-  list_op *op_2 = NULL;
-  list_op *res = NULL;
-    
-  int i,j;
-
-  op_1 = (list_op *)malloc(sizeof(list_op));
-  op_2 = (list_op *)malloc(sizeof(list_op));
-
-  if (!op_1 || !op_2){
-    printf("Erreur allocation mémoire\n");
-    exit(EXIT_FAILURE);
-  }
-
-  head = op_1;
-  op_1->next = op_2;
-
-  /*Remplir deux matrices initiales*/
-  for (i=0; i<SIZE; i++){
-    for (j=0; j<SIZE; j++){
-      if(j%2==0){
-	op_2->mat[i][j] = j;
-	head->mat[i][j] = j;
-      }
-      else {
-	op_2->mat[i][j] = i;
-	head->mat[i][j] = i;
-      }
+    if (!op_1 || !op_2){
+        printf("Erreur allocation mémoire\n");
+        exit(EXIT_FAILURE);
     }
-  }
 
-
-  /*Calcul matriciel*/
-  int k,l = 0;
-  
-  printf("Début de calcul\n");
-  printf("----------------\n");
-  while(l!=ite){
-    res = (list_op *)malloc(sizeof(list_op));
-    op_2->next=res;
-
-    if (!res){
-      printf("Erreur allocation mémoire\n");
-      exit(EXIT_FAILURE);
-    }
-  
-    /*Produit matriciel*/
+    /*Remplir deux matrices initiales*/
     for (i=0; i<SIZE; i++){
-      for (j=0; j<SIZE; j++){
-	res->mat[i][j]=0; 
-	for (k=0;k<SIZE;k++) 
-	  { 
-	    res->mat[i][j] += op_1->mat[i][k] *  op_2->mat[k][j]; 
-	  }
-      }
+        for (j=0; j<SIZE; j++){
+            if(j%2==0){
+                op_2->mat[i][j] = j;
+                op_1->mat[i][j] = j;
+            }else {
+                op_2->mat[i][j] = i;
+                op_1->mat[i][j] = i;
+            }
+        }
     }
-     for (i=0; i<SIZE; i++){
-       for (j=0; j<SIZE; j++){
-	 printf("%d ",res->mat[i][j]);
-       }
-       printf("\n");
-     } 
-     op_1 = op_1->next;
-     op_2 = op_2->next;
-     res= res->next;
 
-     l++;
-     printf("----------------\n");
-  }
-
-
-
-  /*Libération mémoire*/
-
-  while(head->next){
-    res=head;
-    head = head->next;
-    free(res);
-    res=NULL;
-  }
-
-  free(head);
-
-  return 0;
+    list->head = op_1;
+    list->op1 = op_1;
+    list->op1->next = op_2;
+    list->op2 = op_2;
+    list->op2->next = NULL;
 }
 
+void rt_task_nextiter(rt_list *list){
+    /*Calcul matriciel*/
+    int i,j,k = 0;
+    rt_element *res = NULL;
+    
+    res = (rt_element *)malloc(sizeof(rt_element));
+    if (!res){
+        printf("Erreur allocation mémoire\n");
+        exit(EXIT_FAILURE);
+    }
 
+    /*Produit matriciel*/
+    for (i=0; i<SIZE; i++){
+        for (j=0; j<SIZE; j++){
+            res->mat[i][j]=0; 
+            for (k=0;k<SIZE;k++){ 
+                res->mat[i][j] += list->op1->mat[i][k] *  list->op2->mat[k][j]; 
+            }
+        }
+    }
+    
+    for (i=0; i<SIZE; i++){
+        for (j=0; j<SIZE; j++){
+            printf("%d ",res->mat[i][j]);
+        }
+        printf("\n");
+    } 
+    
+    res->next = NULL;
+    list->op2->next = res;
+    list->op1 = list->op1->next;
+    list->op2 = list->op2->next;
+    
+    printf("----------------\n");
+}
+
+void rt_task_destroy(rt_list *list){
+    rt_element *p;
+
+    p = list->head;
+
+    while(p->next){
+        list->head = p->next;
+        free(p);
+        p = list->head;
+    }
+    free(list);
+}
 
 int main(int argc, char* argv[]){
+    
+    rt_list *the_list = NULL;
+    int i = 0;
+    if(argc!= 2){
+        fprintf(stderr, "Usage: %s <nb iterations>\n", argv[0]);
+        exit(1);
+    }
 
-  rt_task(atoi(argv[1]));
+    the_list = (rt_list*)malloc(sizeof(rt_list));
 
-  return 0;
+    rt_task_init(the_list);
 
+    while( i != atoi(argv[1])){
+        rt_task_nextiter(the_list);
+        i++;
+    }
+    rt_task_destroy(the_list);
+
+    return 0;
 }
