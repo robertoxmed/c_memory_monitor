c452ec35b59ca29ab38bd10d640b4c7923f43245
Roberto Medina <roberto.medina-bonilla@inria.fr>
2019-05-15 17:15:22 +0200

    Attacker code refactor

--- src/attack_task.c
+++ src/attack_task.c
@@ -1,148 +1,155 @@
 #include "../include/attack_task.h"
 #include "../include/papi_util.h"
 
-void attack_list_init ( attack_list *al ){
-
-    al->al_nb_elements = 0;
-    al->al_head = NULL;
-    al->al_tail = NULL;
+void attack_list_init(attack_list *al)
+{
+	al->al_nb_elements = 0;
+	al->al_head = NULL;
+	al->al_tail = NULL;
 }
 
-void attack_list_destroy ( attack_list *al ){
-    attack_element *tmp;
-    tmp = al->al_head;
-    while(tmp->ae_next != NULL){
-        al->al_head = tmp->ae_next;
-        free(tmp);
-        tmp = al->al_head;
-    }
-    free(al);
+void attack_list_destroy(attack_list *al)
+{
+	attack_element *tmp;
+	tmp = al->al_head;
+	while (tmp->ae_next != NULL) {
+		al->al_head = tmp->ae_next;
+		free(tmp);
+		tmp = al->al_head;
+	}
+	free(al);
 }
 
-void attack_element_init(attack_element *ae){
-    int i,j =0;
-    for(i=0;i<SIZE;i++){
-        for(j=0;j<SIZE;j++){
-            ae->ae_mat[i][j] = (int) (10*(float)rand()/ RAND_MAX);
-        }
-    }
-    ae->ae_next = NULL;
-    ae->ae_previous = NULL;
+void attack_element_init(attack_element *ae)
+{
+	int i, j = 0;
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++)
+			ae->ae_mat[i][j] = (int) (10*(float)rand()/ RAND_MAX);		
+	}
+	ae->ae_next = NULL;
+	ae->ae_previous = NULL;
 }
 
-void attack_list_add_elt(attack_list *al, attack_element *ae){
-    if(al == NULL || ae == NULL){
-        fprintf(stderr, "Attack list or element not initialized\n");
-        exit(1);
-    }
-    if(al->al_nb_elements == 0){ //First element added => head of the list
-        al->al_head = ae;
-        al->al_tail = ae;
-        ae->ae_next = NULL;
-        ae->ae_previous = NULL;
-        al->al_index[al->al_nb_elements ++] = ae;
-    }else{ //Chain elements
-        al->al_tail->ae_next = ae;
-        ae->ae_previous = al->al_tail;
-        al->al_tail = ae;
-        al->al_index[al->al_nb_elements ++] = ae;
-    }
+void attack_list_add_elt(attack_list *al, attack_element *ae)
+{
+	if (al == NULL || ae == NULL) {
+		fprintf(stderr, "Attack list or element not initialized\n");
+		exit(1);
+	}
+	if (al->al_nb_elements == 0) { //First element added => head of the list
+		al->al_head = ae;
+		al->al_tail = ae;
+		ae->ae_next = NULL;
+		ae->ae_previous = NULL;
+		al->al_index[al->al_nb_elements ++] = ae;
+	} else { //Chain elements
+		al->al_tail->ae_next = ae;
+		ae->ae_previous = al->al_tail;
+		al->al_tail = ae;
+		al->al_index[al->al_nb_elements ++] = ae;
+	}
 }
 
-void attack_list_add_n_elt(attack_list *al){
-    int i;
-    for(i=0;i<INDEX_SIZE;i++){
-        srand(i);
-        attack_element *ae = (attack_element*)malloc(sizeof(attack_element));
-        attack_element_init(ae);
-        attack_list_add_elt(al, ae);
-    }
+void attack_list_add_n_elt(attack_list *al)
+{
+	int i;
+	for (i = 0; i < INDEX_SIZE; i++) {
+		srand(i);
+		attack_element *ae = (attack_element*) malloc(sizeof(attack_element));
+		attack_element_init(ae);
+		attack_list_add_elt(al, ae);
+	}
 }
 
-void attack_element_print(attack_list *al, attack_element *ae){
-    int i, j, k;
-    attack_element ae_tmp;
-    attack_element *ae_tmp2;
-    printf("-----------------\n");
-    for(i=0;i<SIZE;i++){
-        for(j=0;j<SIZE;j++){
-            k = (int)(rand()%INDEX_SIZE);
-            ae_tmp2 = al->al_index[k];
-            memcpy(ae_tmp.ae_mat, ae->ae_mat, sizeof(ae->ae_mat));
-            memcpy(ae->ae_mat, ae_tmp2->ae_mat, sizeof(ae->ae_mat));
-            memcpy(ae_tmp2->ae_mat, ae_tmp.ae_mat, sizeof(ae->ae_mat));
-            ae->ae_mat[i][j] += 10;
-            printf("%g ",ae->ae_mat[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+void attack_element_print(attack_list *al, attack_element *ae)
+{
+	int i, j, k;
+	attack_element ae_tmp;
+	attack_element *ae_tmp2;
 
+	printf("-----------------\n");
+	for (i = 0; i < SIZE; i++) {
+		for(j = 0; j < SIZE; j++) {
+			k = (int) (rand() % INDEX_SIZE);
+			ae_tmp2 = al->al_index[k];
+			memcpy(ae_tmp.ae_mat, ae->ae_mat, sizeof(ae->ae_mat));
+			memcpy(ae->ae_mat, ae_tmp2->ae_mat, sizeof(ae->ae_mat));
+			memcpy(ae_tmp2->ae_mat, ae_tmp.ae_mat, sizeof(ae->ae_mat));
+			ae->ae_mat[i][j] += 10;
+			printf("%g ", ae->ae_mat[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
-void attack_list_iterate(attack_list *al){
-    attack_element *iter;
+void attack_list_iterate(attack_list *al)
+{
+	attack_element *iter;
 
-    while(1){
-        for(iter = al->al_head; iter; iter = iter->ae_next)
-            attack_element_print(al, iter);
-    }
+	while (1) {
+		for (iter = al->al_head; iter; iter = iter->ae_next)
+			attack_element_print(al, iter);
+	}
 }
 
-void attack_list_rand_iterate(attack_list *al){
-    int i, j =0, k, cpt = 0;;
-    while(1){
-        srand(getpid()+i);
-        i = (int)(rand()%al->al_nb_elements);
-        k = (int)(rand()%100) + 1;
-        printf("I = %d, K = %d\n", i, k);
+void attack_list_rand_iterate(attack_list *al)
+{
+	int i, j = 0, k, cpt = 0;
 
-        attack_element *iter = al->al_index[i];
-        while((cpt != k) && (iter != NULL)){
-            attack_element_print(al, iter);
-            iter = iter->ae_next;
-            cpt++;
-        }
-        j++;
-        cpt = 0;
-    }
+	while (1) {
+		srand(getpid()+i);
+		i = (int) (rand() % al->al_nb_elements);
+		k = (int) (rand() % 100) + 1;
+		printf("I = %d, K = %d\n", i, k);
+
+		attack_element *iter = al->al_index[i];
+		while ((cpt != k) && (iter != NULL)) {
+			attack_element_print(al, iter);
+			iter = iter->ae_next;
+			cpt++;
+		}
+		j++;
+		cpt = 0;
+	}
 }
 
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
+	cpu_set_t mask;
 
-    cpu_set_t mask;
+	CPU_ZERO(&mask);
+	CPU_SET(atoi(argv[2]), &mask);
 
-    CPU_ZERO(&mask);
-    CPU_SET(atoi(argv[2]), &mask);
-
-    if(sched_setaffinity(getpid(), sizeof(mask), &mask)){
-        fprintf(stderr, "Sched error: set affinity\n");
-        exit(16);
-    }
-    attack_list *al = (attack_list*)malloc(sizeof(attack_list));
-    al->al_index = (attack_element**)malloc(INDEX_SIZE *sizeof(attack_element*));
+	if (sched_setaffinity(getpid(), sizeof(mask), &mask)) {
+		fprintf(stderr, "Sched error: set affinity\n");
+		exit(16);
+	}
+	attack_list *al = (attack_list*) malloc(sizeof(attack_list));
+	al->al_index = (attack_element**) malloc(INDEX_SIZE *sizeof(attack_element*));
     
-    //Add the process to the table used by the hypervisor    
-    attack_list_init(al);
-    fprintf(stderr, "Attack task (%d) > al->nb_elts %d In core %d\n", getpid(), al->al_nb_elements, atoi(argv[2]));
-    attack_list_add_n_elt(al);
-    fprintf(stderr, "Attack task (%d) > Allocation done: al->nb_elts %d\n", getpid(), al->al_nb_elements);
+	//Add the process to the table used by the hypervisor    
+	attack_list_init(al);
+	fprintf(stderr, "Attack task (%d) > al->nb_elts %d In core %d\n", getpid(), al->al_nb_elements, atoi(argv[2]));
+	attack_list_add_n_elt(al);
+	fprintf(stderr, "Attack task (%d) > Allocation done: al->nb_elts %d\n", getpid(), al->al_nb_elements);
 
-    if(argc == 3){
-        if(atoi(argv[1]) == 1){
-            fprintf(stderr, "Attacker will use random iteration\n");
-            attack_list_rand_iterate(al);
-        }else{
-            fprintf(stderr, "Attacker will use linear iteration\n");
-            attack_list_iterate(al);
-        }
-    }else{
-        fprintf(stderr, "Attacker will use random iteration\n");
-        attack_list_rand_iterate(al);
-    }
+	if (argc == 3) {
+		if (atoi(argv[1]) == 1) {
+			fprintf(stderr, "Attacker will use random iteration\n");
+			attack_list_rand_iterate(al);
+		} else {
+			fprintf(stderr, "Attacker will use linear iteration\n");
+			attack_list_iterate(al);
+		}
+	} else {
+		fprintf(stderr, "Attacker will use random iteration\n");
+		attack_list_rand_iterate(al);
+	}
 
-    attack_list_destroy(al);
+	attack_list_destroy(al);
     
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- src/attack_task2.c
+++ src/attack_task2.c
@@ -1,165 +1,172 @@
 #include "../include/attack_task.h"
 #include "../include/papi_util.h"
 
-void handler(int sig){
-    fprintf(stderr, "Attack Task (%d) > Freeing memory\n", getpid());
-    fflush(stderr);
-    exit(0);
+void handler(int sig)
+{
+	fprintf(stderr, "Attack Task (%d) > Freeing memory\n", getpid());
+	fflush(stderr);
+	exit(0);
 }
 
-void attack_list_init ( attack_list *al ){
-
-    al->al_nb_elements = 0;
-    al->al_head = NULL;
-    al->al_tail = NULL;
+void attack_list_init(attack_list *al)
+{
+	al->al_nb_elements = 0;
+	al->al_head = NULL;
+	al->al_tail = NULL;
 }
 
-void attack_list_destroy ( attack_list *al ){
-    attack_element *tmp;
-    tmp = al->al_head;
-    while(tmp->ae_next != NULL){
-        al->al_head = tmp->ae_next;
-        free(tmp);
-        tmp = al->al_head;
-    }
-    free(al);
+void attack_list_destroy(attack_list *al)
+{
+	attack_element *tmp;
+	tmp = al->al_head;
+	while(tmp->ae_next != NULL){
+		al->al_head = tmp->ae_next;
+		free(tmp);
+		tmp = al->al_head;
+	}
+	free(al);
 }
 
-void attack_element_init(attack_element *ae){
-    int i,j =0;
-    for(i=0;i<SIZE;i++){
-        for(j=0;j<SIZE;j++){
-            ae->ae_mat[i][j] = (int) (10*(float)rand()/ RAND_MAX);
-        }
-    }
-    ae->ae_next = NULL;
-    ae->ae_previous = NULL;
+void attack_element_init(attack_element *ae)
+{
+	int i, j = 0;
+
+	for (i = 0; i < SIZE; i++) {
+	        for (j = 0; j < SIZE; j++) {
+			ae->ae_mat[i][j] = (int) (10*(float)rand()/ RAND_MAX);
+		}
+	}
+	ae->ae_next = NULL;
+	ae->ae_previous = NULL;
 }
 
-void attack_list_add_elt(attack_list *al, attack_element *ae){
-    if(al == NULL || ae == NULL){
-        fprintf(stderr, "Attack list or element not initialized\n");
-        exit(1);
-    }
-    if(al->al_nb_elements == 0){ //First element added => head of the list
-        al->al_head = ae;
-        al->al_tail = ae;
-        ae->ae_next = NULL;
-        ae->ae_previous = NULL;
-        al->al_index[al->al_nb_elements ++] = ae;
-    }else{ //Chain elements
-        al->al_tail->ae_next = ae;
-        ae->ae_previous = al->al_tail;
-        al->al_tail = ae;
-        al->al_index[al->al_nb_elements ++] = ae;
-    }
+void attack_list_add_elt(attack_list *al, attack_element *ae)
+{
+	if (al == NULL || ae == NULL) {
+		fprintf(stderr, "Attack list or element not initialized\n");
+		exit(1);
+	}
+	if(al->al_nb_elements == 0){ //First element added => head of the list
+		al->al_head = ae;
+		al->al_tail = ae;
+		ae->ae_next = NULL;
+		ae->ae_previous = NULL;
+		al->al_index[al->al_nb_elements ++] = ae;
+	} else { //Chain elements
+		al->al_tail->ae_next = ae;
+		ae->ae_previous = al->al_tail;
+		al->al_tail = ae;
+		al->al_index[al->al_nb_elements ++] = ae;
+	}
 }
 
-void attack_list_add_n_elt(attack_list *al){
-    int i;
-    for(i=0;i<INDEX_SIZE;i++){
-        srand(i);
-        attack_element *ae = (attack_element*)malloc(sizeof(attack_element));
-        attack_element_init(ae);
-        attack_list_add_elt(al, ae);
-    }
+void attack_list_add_n_elt(attack_list *al)
+{
+	int i;
+
+	for (i = 0; i <I NDEX_SIZE; i++) {
+		srand(i);
+		attack_element *ae = (attack_element*) malloc(sizeof(attack_element));
+		attack_element_init(ae);
+		attack_list_add_elt(al, ae);
+	}
 }
 
-void attack_element_print(attack_list *al, attack_element *ae){
-    int i, j, k;
-    attack_element ae_tmp;
-    attack_element *ae_tmp2;
-    printf("-----------------\n");
-    for(i=0;i<SIZE;i++){
-        for(j=0;j<SIZE;j++){
-            k = (int)(rand()%INDEX_SIZE);
-            ae_tmp2 = al->al_index[k];
-            memcpy(ae_tmp.ae_mat, ae->ae_mat, sizeof(ae->ae_mat));
-            memcpy(ae->ae_mat, ae_tmp2->ae_mat, sizeof(ae->ae_mat));
-            memcpy(ae_tmp2->ae_mat, ae_tmp.ae_mat, sizeof(ae->ae_mat));
-            ae->ae_mat[i][j] += 10;
-            printf("%g ",ae->ae_mat[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+void attack_element_print(attack_list *al, attack_element *ae)
+{
+	int i, j, k;
+	attack_element ae_tmp;
+	attack_element *ae_tmp2;
 
+	printf("-----------------\n");
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			k = (int) (rand()%INDEX_SIZE);
+			ae_tmp2 = al->al_index[k];
+			memcpy(ae_tmp.ae_mat, ae->ae_mat, sizeof(ae->ae_mat));
+			memcpy(ae->ae_mat, ae_tmp2->ae_mat, sizeof(ae->ae_mat));
+			memcpy(ae_tmp2->ae_mat, ae_tmp.ae_mat, sizeof(ae->ae_mat));
+			ae->ae_mat[i][j] += 10;
+			printf("%g ",ae->ae_mat[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
-void attack_list_iterate(attack_list *al){
-    attack_element *iter;
+void attack_list_iterate(attack_list *al)
+{
+	attack_element *iter;
 
-    while(1){
-        for(iter = al->al_head; iter; iter = iter->ae_next){
-                attack_element_print(al, iter);
-        }
-    }
-}
-
-
-void attack_list_rand_iterate(attack_list *al){
-    int i, j =0, k, cpt = 0;;
-    while(1){
-        srand(getpid()+i);
-        i = (int)(rand()%al->al_nb_elements);
-        k = (int)(rand()%10) + 1;
-        printf("I = %d, K = %d\n", i, k);
-
-        attack_element *iter = al->al_index[i];
-        while((cpt != k) && (iter != NULL)){
-            attack_element_print(al, iter);
-            iter = iter->ae_next;
-            cpt++;
-        }
-        j++;
-        cpt = 0;
-    }
+	while (1) {
+		for (iter = al->al_head; iter; iter = iter->ae_next)
+			attack_element_print(al, iter);
+	}
 }
 
 
-int main(int argc, char **argv){
+void attack_list_rand_iterate(attack_list *al)
+{
+	int i, j = 0, k, cpt = 0;
+	while (1) {
+		srand(getpid()+i);
+		i = (int) (rand() % al->al_nb_elements);
+		k = (int) (rand() % 10) + 1;
+		printf("I = %d, K = %d\n", i, k);
 
-    struct sigaction sa;
+		attack_element *iter = al->al_index[i];
+		while ((cpt != k) && (iter != NULL)) {
+			attack_element_print(al, iter);
+			iter = iter->ae_next;
+			cpt++;
+		}
+		j++;
+		cpt = 0;
+	}
+}
 
-    memset(&sa, 0, sizeof(struct sigaction));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = handler;
-    sigaction(SIGUSR1, &sa, NULL);
 
+int main(int argc, char **argv)
+{
+	struct sigaction sa;
 
-    attack_list *al = (attack_list*)malloc(sizeof(attack_list));
-    attack_list *al_2 = (attack_list*)malloc(sizeof(attack_list));
-    al->al_index = (attack_element**)malloc(INDEX_SIZE *sizeof(attack_element*));
-    al_2->al_index = (attack_element**)malloc(INDEX_SIZE * sizeof(attack_element*));
+	memset(&sa, 0, sizeof(struct sigaction));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	sigaction(SIGUSR1, &sa, NULL);
+
+	attack_list *al = (attack_list*) malloc(sizeof(attack_list));
+	attack_list *al_2 = (attack_list*) malloc(sizeof(attack_list));
+	al->al_index = (attack_element**) malloc(INDEX_SIZE * sizeof(attack_element*));
+	al_2->al_index = (attack_element**) malloc(INDEX_SIZE * sizeof(attack_element*));
     
-    //Add the process to the table used by the hypervisor    
-    attack_list_init(al);
-    attack_list_init(al_2);
-    fprintf(stderr, "Attack task (%d) > al->nb_elts %d\n", getpid(), al->al_nb_elements);
-    attack_list_add_n_elt(al);
-    attack_list_add_n_elt(al_2);
-    fprintf(stderr, "Attack task (%d) > Allocation done: al->nb_elts %d\n", getpid(), al->al_nb_elements);
+	//Add the process to the table used by the hypervisor    
+	attack_list_init(al);
+	attack_list_init(al_2);
+	fprintf(stderr, "Attack task (%d) > al->nb_elts %d\n", getpid(), al->al_nb_elements);
+	attack_list_add_n_elt(al);
+	attack_list_add_n_elt(al_2);
+	fprintf(stderr, "Attack task (%d) > Allocation done: al->nb_elts %d\n", getpid(), al->al_nb_elements);
 
+	//Notify the scheduler
+	kill(getppid(), SIGCONT);
 
-    //Notify the scheduler
-    kill(getppid(), SIGCONT);
+	if (argc == 2) {
+		if (atoi(argv[1]) == 1) {
+			fprintf(stderr, "Attacker will use random iteration\n");
+			attack_list_rand_iterate(al);
+		} else {
+			fprintf(stderr, "Attacker will use linear iteration\n");
+			attack_list_iterate(al);
+		}
+	} else {
+		fprintf(stderr, "Attacker will use linear iteration\n");
+		attack_list_iterate(al);
+	}
 
-    if(argc == 2){
-        if(atoi(argv[1]) == 1){
-            fprintf(stderr, "Attacker will use random iteration\n");
-            attack_list_rand_iterate(al);
-        }else{
-            fprintf(stderr, "Attacker will use linear iteration\n");
-            attack_list_iterate(al);
-        }
-    }else{
-        fprintf(stderr, "Attacker will use linear iteration\n");
-        attack_list_iterate(al);
-    }
+	attack_list_destroy(al);
+	attack_list_destroy(al_2);
 
-    attack_list_destroy(al);
-    attack_list_destroy(al_2);
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
